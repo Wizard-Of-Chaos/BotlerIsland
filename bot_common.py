9e36fd9568cc58f8d1e7cfd2e50d59975767f9bd
virtuNat <19920458+virtuNat@users.noreply.github.com>
2021-04-30 23:03:10 +0800

    Streamline main better

--- bot_common.py
+++ bot_common.py
@@ -27,8 +27,8 @@ CONST_AUTHOR = (125433170047795200, 257144766901256192) # 9, WoC
 
 
 def main():
-    with open('token.dat', 'r') as tokenfile:
+    with (open('token.dat', 'r') as tokenfile,
+        guild_config, member_stalker, stats_tracker, stored_suggestions,
+        ):
         raw = tokenfile.read().strip()
-        token = ''.join(chr(int(''.join(c), 16)) for c in zip(*[iter(raw)]*2))
-    with guild_config, member_stalker, stats_tracker, stored_suggestions:
-        bot.run(token)
+        bot.run(''.join(chr(int(''.join(c), 16)) for c in zip(*[iter(raw)]*2)))
